6cdc07535db9ca29a49819a94e0ed30584ca2804
NeilZheng <a1onexiang2@qq.com>
2017-06-27 15:34:48 +0800

    feat(utils) : remove some redundant functions about Toast

--- core/src/main/java/android/baseframework/core/utils/ToastUtils.kt
+++ core/src/main/java/android/baseframework/core/utils/ToastUtils.kt
@@ -3,6 +3,8 @@ package android.baseframework.core.utils
 import android.app.Activity
 import android.app.Dialog
 import android.app.Fragment
+import android.baseframework.core.BaseCoreApp
+import android.baseframework.core.config.Global
 import android.content.Context
 import android.support.annotation.StringRes
 import android.text.TextUtils
@@ -14,51 +16,19 @@ import android.support.v4.app.Fragment as SupportFragment
  * Created by Neil Zheng on 2017/6/19.
  */
 
-private var mLastShowString: String? = null
-private var mLastShowTimestamp: Long = 0
-private val DEFAULT_TOAST_INTERVAL = 2000
+@Volatile private var mLastShowString: String? = null
+@Volatile private var mLastShowTimestamp = 0L
+private val DEFAULT_TOAST_INTERVAL = 2000L
 
-fun Activity.showToast(message: String?, duration: Int = Toast.LENGTH_SHORT) {
-    doShowToast(this, message, duration)
+fun showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    doShowToast(Global.context!!, message, duration)
 }
 
-fun Fragment.showToast(message: String?, duration: Int = Toast.LENGTH_SHORT) {
-    doShowToast(this.context, message, duration)
+fun showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
+    showToast(Global.context!!.getString(resId), duration)
 }
 
-fun SupportFragment.showToast(message: String?, duration: Int = Toast.LENGTH_SHORT) {
-    doShowToast(this.context, message, duration)
-}
-
-fun Dialog.showToast(message: String?, duration: Int = Toast.LENGTH_SHORT) {
-    doShowToast(this.context, message, duration)
-}
-
-fun PopupWindow.showToast(message: String?, duration: Int = Toast.LENGTH_SHORT) {
-    doShowToast(this.contentView.context, message, duration)
-}
-
-fun Activity.showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
-    showToast(this.getString(resId), duration)
-}
-
-fun Fragment.showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
-    showToast(this.getString(resId), duration)
-}
-
-fun SupportFragment.showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
-    showToast(this.getString(resId), duration)
-}
-
-fun Dialog.showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
-    showToast(this.context.getString(resId), duration)
-}
-
-fun PopupWindow.showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
-    showToast(this.contentView.context.getString(resId), duration)
-}
-
-private fun doShowToast(context: Context, message: String?, duration: Int) {
+private fun doShowToast(context: Context, message: String, duration: Int) {
     if (!isToastShowing(message)) {
         Toast.makeText(context, message, duration).show()
         mLastShowTimestamp = System.currentTimeMillis()
